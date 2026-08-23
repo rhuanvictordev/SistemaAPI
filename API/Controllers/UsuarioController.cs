af0d6f85c581ef04b57748dc40b5dbd7cb86a956
Rhuan Victor <rhuansoliveira1072@gmai.com>
2026-08-22 21:03:14 -0300

    ajuste

--- API/Controllers/UsuarioController.cs
+++ API/Controllers/UsuarioController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RestauranteAPI.Models;
 
 namespace RestauranteAPI.API.Controllers
 {
@@ -8,9 +9,11 @@ namespace RestauranteAPI.API.Controllers
     public class UsuarioController : ControllerBase
     {
         [HttpGet]
-        public List<string> getUsers()
+        public List<Usuario> getUsers()
         { 
-            var list = new List<string>() {"rhuan","ciclano","beltrano"};
+            var list = new List<Usuario>();
+            Usuario u = new Usuario() { Id = 1, Nome = "Rhuan Victor", SenhaHash = "12345678", Email = "rhuan@email.com" };
+            list.Add(u);
             return list;
         }
     }
