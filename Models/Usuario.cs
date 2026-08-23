0a3c4dd96206a5eff55ffe04706c8808ec7a3739
Rhuan Victor <rhuansoliveira1072@gmai.com>
2026-08-22 21:15:18 -0300

    a

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,6 +1,38 @@
-﻿namespace RestauranteAPI.Models
+﻿using RestauranteAPI.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestauranteAPI.Models
 {
-    public class Usuario
+    public class Usuario : IModel
     {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string SenhaHash { get; set; }
+
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Load(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(object obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
