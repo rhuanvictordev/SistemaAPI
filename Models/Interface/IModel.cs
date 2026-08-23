742a96a83c44b60b8acaaa36ee4e2c2dcabcb2f9
Rhuan Victor <rhuansoliveira1072@gmai.com>
2026-08-22 21:03:26 -0300

    ajuste

---
+++ Models/Interface/IModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestauranteAPI.Models.Interface
+{
+    public interface IModel
+    {
+        public void Save();
+        public bool Load(long id);
+        public bool Delete(long id);
+        public bool Update(Object obj);
+    }
+}
