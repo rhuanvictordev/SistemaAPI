e50048a98a5086b61f2a86e581cd79959b717f41
Rhuan Victor <rhuansoliveira1072@gmai.com>
2026-09-07 18:38:18 -0300

    ajuste tailwind

---
+++ wwwroot/js/login/login.js
@@ -0,0 +1,22 @@
+﻿
+function switchTheme() {
+    const temaSalvo = localStorage.getItem("theme") || "light";
+    document.documentElement.classList.remove("light", "dark");
+
+    if (temaSalvo == "light") {
+        localStorage.setItem("theme", "dark");
+        document.documentElement.classList.add("dark");
+    } else {
+        localStorage.setItem("theme", "light");
+        document.documentElement.classList.add("light");
+    }
+}
+
+function loadTheme() {
+    const temaSalvo = localStorage.getItem("theme") || "light";
+
+    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.add(temaSalvo);
+}
+
+window.addEventListener("load", loadTheme);
